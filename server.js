088d37dcb0afa6f3f3f670db3d69bb9c6f439abe
Stefan <stefan.paiu.0595@gmail.com>
2023-05-31 12:39:54 +0100

    Finished backed integration

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/signin', (req, res) => {
 
     if(req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password){
-            res.json('success')
+            res.json(database.users[0])
     }
     else {
         res.status(400).json('Error logging in.')
@@ -69,7 +69,6 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     });
